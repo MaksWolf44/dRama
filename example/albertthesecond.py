4cb01516eba2cee2b1b3c50b5e4f6fb1230050be
MaksWolf44 <81531151+MaksWolf44@users.noreply.github.com>
2024-04-19 16:04:55 +0200

    Add files via upload

--- example/albertthesecond.py
+++ example/albertthesecond.py
@@ -1,27 +1,28 @@
 '''
 This is an exemplary usage of the "drama" script.
- It loops through the files in the current directory and produces a plot of average absolute values.
+It loops through the files in the current directory and produces a plot of som of absolute values.
 For testing, you can use the accompanying .xvg input files that its set up to work with. 
 '''
 
 
-from dramaApr5 import * 
+from dramaApr19 import drama 
 deviation = 0
-import os
-          
+
+import os 
 katalog = "C:\\Users\\ysrtxn\\Desktop\\barys\\gify\\" 
+#katalog = os.getcwd()
 os.chdir(katalog)
 import time
 
 deviations = []
 
-for i in range(2,70):
+for i in range(2,71):
     start = time.time()
     nameSS = "t_"+ str(i) + ".xvg"
     nameRR = "t_" +str(1)+ ".xvg"   
     #nameRR = "t_1.xvg"
     bins = 3
-    devo = drama(nameSS,nameRR,binSize=3,saveDir= "PIcture19",borderline=20,animatedGif=True,plotData=True,tfs=15)
+    devo = drama(nameSS,nameRR,binSize=3,saveDir= "B2",borderline=0.003,animatedGif=False,plotRaw=False,plotData=False,tfs=15)
     deviations.append([i,devo])
     end = time.time()
     print("time=" + str(end - start))    
@@ -41,22 +42,23 @@ def saveTsv(tsvfilename,arraytosave):                      # function that  saSe
         tsvW.writerow(linia)            
     fileTSV.close()
     
-  
+scaleAxis = 30  
 import matplotlib.pyplot as plt       # used to plot the data       
-tfs = 20  
+tfs = 18 
 lfs = tfs + 2
 print(deviationsc[:,1]) 
 jadrodewiacji = np.ones(3)/3
 runigDevatios = np.convolve(deviationsc[:,1], jadrodewiacji, mode='valid')  
 runingX = np.convolve(deviationsc[:,0],jadrodewiacji,mode = 'valid')
-plt.scatter(deviationsc[:,0]+25,deviationsc[:,1],s=5,c="black")
-plt.plot(runingX+25,runigDevatios,color="black")
-#plt.gca().set_aspect(420)
-plt.xticks(fontsize = tfs)
-#plt.xlim(24, 96)
-plt.yticks(fontsize = tfs)
-plt.xlabel("X axis title", fontsize = lfs)
-plt.ylabel("Absolute average", fontsize = lfs)
+plt.scatter(deviationsc[:,0]+scaleAxis,deviationsc[:,1],s=5,c="black")
+plt.plot(runingX+scaleAxis,runigDevatios,color="black")
+plt.gca().set_aspect(210)
+plt.xticks([40,60,80,100],fontsize = tfs)
+plt.xlim(29, 101)
+plt.yticks([.24,.32,.40],fontsize = tfs)
+plt.xlabel("$T[^o C]$", fontsize = lfs)
+plt.ylabel("SAD", fontsize = lfs)
+#plt.subplots_adjust(left=0.19)
 
-plt.savefig(katalog+ "absolutPlotR19.jpg") 
+plt.savefig(katalog+ "SAD_PlotB1.png",bbox_inches='tight') 
 plt.cla()
